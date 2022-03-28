f7ed215926aef06568e0f3c1bf01908e721f5194
shooterfei <shooterfei@163.com>
2022-03-28 13:17:22 +0800

    nvim-qt 的字体设置

--- lua/custom/custom_init.lua
+++ lua/custom/custom_init.lua
@@ -4,6 +4,9 @@
 -- 禁用多光标警告
 vim.g.VM_show_warnings = 0
 
+-- nvim-qt 的字体设置， 需先安装nerd-font 字体
+vim.opt.guifont="Hack Nerd Font Mono"
+
 -- 设置asynctasks窗口排版
 vim.g.asynctasks_term_pos = "bottom"
 vim.g.asynctasks_term_rows = 15
